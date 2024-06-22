95d6f8c82185f92af50c1f91427cf77700be2b79
woongaaaa <woonge0930@naver.com>
2024-06-22 16:45:36 +0900

    프로젝트 파일 추가 및 스토리보드 파일 삭제

---
+++ STEW/STEW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STEW
+//
+//  Created by 황채웅 on 6/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
