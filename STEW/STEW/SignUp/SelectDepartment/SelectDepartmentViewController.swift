4364e9818e2c5be34dadf45db282fd29860ca424
woongaaaa <woonge0930@naver.com>
2024-07-12 02:34:03 +0900

    로그인 관련 폴더 및 파일 추가

---
+++ STEW/STEW/SignUp/SelectDepartment/SelectDepartmentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectDepartmentViewController.swift
+//  STEW
+//
+//  Created by 황채웅 on 7/12/24.
+//
+
+import UIKit
+
+class SelectDepartmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
