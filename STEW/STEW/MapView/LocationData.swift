99f1f65033fa33b48b9e605416cf203bee338bcb
woongaaaa <woonge0930@naver.com>
2024-07-12 02:15:40 +0900

    단위, 업체 데이터 추가

---
+++ STEW/STEW/MapView/LocationData.swift
@@ -0,0 +1,8 @@
+//
+//  LocationData.swift
+//  STEW
+//
+//  Created by 황채웅 on 7/11/24.
+//
+
+import Foundation
---
+++ STEW/STEW/UnitData.swift
@@ -0,0 +1,8 @@
+//
+//  UnitData.swift
+//  STEW
+//
+//  Created by 황채웅 on 7/12/24.
+//
+
+import Foundation
