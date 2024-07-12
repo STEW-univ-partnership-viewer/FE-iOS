cf07bffb4bcf62e1becd9b1f5ba302455a5af139
woongaaaa <woonge0930@naver.com>
2024-07-12 19:08:07 +0900

    ListViewCollectionViewCell xib로 UI 구현

---
+++ STEW/STEW/ListView/ListViewCollectionView/ListViewCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewCollectionViewCell.swift
+//  STEW
+//
+//  Created by 황채웅 on 7/12/24.
+//
+
+import UIKit
+
+class ListViewCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
